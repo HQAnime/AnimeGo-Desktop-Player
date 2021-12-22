5903aeb13c9fec5f199266f7d71a7b04f302a1c4
Yiheng (NUC) <development.henryquan@gmail.com>
2021-12-22 22:13:26 +1100

    run system command

--- ffi/system.c
+++ ffi/system.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char **argv)
 {
@@ -8,6 +9,7 @@ int main(int argc, char **argv)
     printf("Mac OS\n");
     #elif __unix__
     printf("Linux\n");
+    system("ls");
     #endif
 
     return 0;
