9e39018193669d6bc2d05a18b117cfd8d4ffd7c9
Yiheng <development.henryquan@gmail.com>
2022-09-12 22:23:27 +1000

    working on windows build

--- webview/README.md
+++ webview/README.md
@@ -5,7 +5,9 @@ g++ player.cpp -std=c++11 -Ilibs/webview $(pkg-config --cflags --libs gtk+-3.0 w
 # macOS
 g++ player.cpp -std=c++11 -framework WebKit && ./a.out
 # Windows/MinGW
-g++ player.cpp -std=c++17 -mwindows -Ilibs/webview -Ilibs/webview2/build/native/include -Llibs/webview2/build/native/x64 -lWebView2Loader.dll -lole32 -lshell32 -lshlwapi -luser32 && ./a.exe
+g++ player.cpp -std=c++17 -mwindows -I"libs/webview2/build/native/include" -L"libs/webview2/build/native/x64" -l"WebView2Loader.dll"  -lole32 -lshell32 -lshlwapi -luser32
 ```
 
 https://github.com/webview/webview
+
+* For Windows, you may need the latest compiler to support C++17.
--- webview/player.cpp
+++ webview/player.cpp
@@ -1,45 +1,56 @@
-#include "webview.h"
 #include <iostream>
+#include "webview.h"
+
 
 #ifdef _WIN32
-int WINAPI WinMain(HINSTANCE hInt, HINSTANCE hPrevInst, LPSTR lpCmdLine,
+int WINAPI WinMain(HINSTANCE hInt,
+                   HINSTANCE hPrevInst,
+                   LPSTR lpCmdLine,
                    int nCmdShow) {
+    int argc = __argc;
+    char** argv = __argv;
+    // print out
+    for (int i = 0; i < argc; i++) {
+        std::cout << argv[i] << std::endl;
+    }
 #else
 int main(int argc, char* argv[]) {
 #endif
-  if (argc < 2) {
-      std::cout << "Usage: " << argv[0] << " URL" << std::endl;
-      return 1;
-  }
-  webview::webview w(true, nullptr);
-  w.set_title("AnimeGo");
-  w.set_size(1280, 720, WEBVIEW_HINT_NONE);
-  // send console.log to stdout
-  // remove all video ads with autoplay
-  // w.init("document.addEventListener('DOMContentLoaded', function() {"
-  //         "var autoplay = document.querySelectorAll('iframe');"
-  //         "window.logger(autoplay);"
-  //         "for (var i = 0; i < autoplay.length; i++) {"
-  //           "autoplay[i].remove();"
-  //         "}"
-  //       "});");
-  // allow send method when counter is less than 10
-  w.init("XMLHttpRequest.prototype.orgSend = XMLHttpRequest.prototype.send;"
-          "var counter = 0;"
-          "XMLHttpRequest.prototype.send = function() {"
-            "window.logger('send');"
-            // block future requests to block ads hopefully
-            "if (counter < 4) {"
-              "counter++;"
-              "window.logger(counter);"
-              "this.orgSend.apply(this, arguments);"
-            "}"
-          "};");
-  w.bind("logger", [&](const std::string &msg) {
-    std::cout << msg << std::endl;
-    return "";
-  });
-  w.navigate(argv[1]);
-  w.run();
-  return 0;
+    if (argc < 2) {
+        std::cout << "Usage: " << argv[0] << " URL" << std::endl;
+        return 1;
+    }
+
+    webview::webview w(true, nullptr);
+    w.set_title("AnimeGo");
+    w.set_size(1280, 720, WEBVIEW_HINT_NONE);
+    // send console.log to stdout
+    // remove all video ads with autoplay
+    // w.init("document.addEventListener('DOMContentLoaded', function() {"
+    //         "var autoplay = document.querySelectorAll('iframe');"
+    //         "window.logger(autoplay);"
+    //         "for (var i = 0; i < autoplay.length; i++) {"
+    //           "autoplay[i].remove();"
+    //         "}"
+    //       "});");
+    // allow send method when counter is less than 10
+    w.init(
+        "XMLHttpRequest.prototype.orgSend = XMLHttpRequest.prototype.send;"
+        "var counter = 0;"
+        "XMLHttpRequest.prototype.send = function() {"
+        "window.logger('send');"
+        // block future requests to block ads hopefully
+        "if (counter < 4) {"
+        "counter++;"
+        "window.logger(counter);"
+        "this.orgSend.apply(this, arguments);"
+        "}"
+        "};");
+    w.bind("logger", [&](const std::string& msg) {
+        std::cout << msg << std::endl;
+        return nullptr;
+    });
+    w.navigate(argv[1]);
+    w.run();
+    return 0;
 }
