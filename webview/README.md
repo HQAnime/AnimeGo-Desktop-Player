65ff3939cba72b4ca7911ea8f09183902d5ce516
Yiheng <development.henryquan@gmail.com>
2022-09-12 23:17:45 +1000

    working on windows kinda

--- webview/README.md
+++ webview/README.md
@@ -5,7 +5,7 @@ g++ player.cpp -std=c++11 -Ilibs/webview $(pkg-config --cflags --libs gtk+-3.0 w
 # macOS
 g++ player.cpp -std=c++11 -framework WebKit && ./a.out
 # Windows/MinGW
-g++ player.cpp -std=c++17 -mwindows -I"libs/webview2/build/native/include" -L"libs/webview2/build/native/x64" -l"WebView2Loader.dll"  -lole32 -lshell32 -lshlwapi -luser32
+g++ player.cpp -std=c++17 -mwindows -I"libs/webview2/build/native/include" -L"libs/webview2/build/native/x64" -l"WebView2Loader.dll" -lole32 -lshell32 -lshlwapi -luser32
 ```
 
 https://github.com/webview/webview
--- webview/player.cpp
+++ webview/player.cpp
@@ -1,29 +1,25 @@
-#include <iostream>
 #include "webview.h"
 
+#define WINDOW_WIDTH 1280
+#define WINDOW_HEIGHT 720
 
-#ifdef _WIN32
-int WINAPI WinMain(HINSTANCE hInt,
-                   HINSTANCE hPrevInst,
-                   LPSTR lpCmdLine,
-                   int nCmdShow) {
-    int argc = __argc;
-    char** argv = __argv;
-    // print out
-    for (int i = 0; i < argc; i++) {
-        std::cout << argv[i] << std::endl;
-    }
-#else
 int main(int argc, char* argv[]) {
-#endif
-    if (argc < 2) {
-        std::cout << "Usage: " << argv[0] << " URL" << std::endl;
-        return 1;
-    }
-
     webview::webview w(true, nullptr);
     w.set_title("AnimeGo");
-    w.set_size(1280, 720, WEBVIEW_HINT_NONE);
+    w.set_size(WINDOW_WIDTH, WINDOW_HEIGHT, WEBVIEW_HINT_NONE);
+
+#ifdef _WIN32
+    // setup console for printf
+    AttachConsole(ATTACH_PARENT_PROCESS);
+    freopen("CONOUT$", "w", stdout);
+
+    // move the window to the center of the screen
+    HWND hwnd = (HWND)w.window();
+    int x = (GetSystemMetrics(SM_CXSCREEN) - WINDOW_WIDTH) / 2;
+    int y = (GetSystemMetrics(SM_CYSCREEN) - WINDOW_HEIGHT) / 2;
+    SetWindowPos(hwnd, 0, x, y, 0, 0, SWP_NOZORDER | SWP_NOSIZE);
+#endif
+
     // send console.log to stdout
     // remove all video ads with autoplay
     // w.init("document.addEventListener('DOMContentLoaded', function() {"
@@ -33,24 +29,51 @@ int main(int argc, char* argv[]) {
     //           "autoplay[i].remove();"
     //         "}"
     //       "});");
-    // allow send method when counter is less than 10
+
+    // override window.open method to not do anything
+    w.init("window.open = function() {};");
+    // allow send method when counter is less than 4
+    // w.init(
+    //     "XMLHttpRequest.prototype.orgSend = XMLHttpRequest.prototype.send;"
+    //     "var counter = 0;"
+    //     "XMLHttpRequest.prototype.send = function(body) {"
+    //     "window.logger(body);"
+    //     "window.logger('send');"
+    //     // block future requests to block ads hopefully
+    //     "if (counter < 5) {"
+    //     "counter++;"
+    //     "window.logger(counter);"
+    //     "this.orgSend.apply(this, arguments);"
+    //     "}"
+    //     "};"
+    // );
+    // override open method to not do anything
     w.init(
-        "XMLHttpRequest.prototype.orgSend = XMLHttpRequest.prototype.send;"
+        "XMLHttpRequest.prototype.orgOpen = XMLHttpRequest.prototype.open;"
         "var counter = 0;"
-        "XMLHttpRequest.prototype.send = function() {"
-        "window.logger('send');"
-        // block future requests to block ads hopefully
-        "if (counter < 4) {"
+        "XMLHttpRequest.prototype.open = function(method, url, async, user, "
+        "password) {"
+        "window.logger(method, url, async, user, password);"
+        "if (counter < 5) {"
         "counter++;"
         "window.logger(counter);"
-        "this.orgSend.apply(this, arguments);"
+        "this.orgOpen.apply(this, arguments);"
         "}"
+        "if (url.includes('.m3u8')) {"
+        "this.orgOpen.apply(this, arguments);"
         "};");
     w.bind("logger", [&](const std::string& msg) {
-        std::cout << msg << std::endl;
-        return nullptr;
+        printf("log: %s\n", msg.c_str());
+        return "";
     });
-    w.navigate(argv[1]);
+
+    if (argc < 2) {
+        // load the usage so the program will still run
+        w.set_html("<center><h1>Usage: player.exe &lt;url&gt;</h1></center>");
+    } else {
+        w.navigate(argv[1]);
+    }
+
     w.run();
     return 0;
 }
